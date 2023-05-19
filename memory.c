66266e7aa7e718d844f51a0e934df8043c1e6305
Chidiebere Chukwuma <chukschiboy@gmail.com>
2023-05-19 14:58:52 -0700

    creating the main entry file and the memory script file

---
+++ memory.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+
+/**
+ * bfree - frees a pointer and NULLs the address
+ * @ptr: address of the pointer to be free
+ * Return: 1 if free, else 0.
+ */
+int bfree(void **ptr)
+{
+	if (ptr && *ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
+}
