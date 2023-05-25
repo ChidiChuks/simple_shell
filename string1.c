a54141be9158209f4d18d0f8bbd54a5197025e15
Chidiebere Chukwuma <chukschiboy@gmail.com>
2023-05-25 00:22:18 -0700

    update on debug

--- string1.c
+++ string1.c
@@ -22,7 +22,7 @@ char *_strcpy(char *dest, char *src)
 /**
  * _strdup - duplicates a string
  * @str: the string to duplicate
- * Return: pointer to the duplicated string
+ * Return: pointer to the duplicated string (NULL)
 */
 char *_strdup(const char *str)
 {
